6230b52917bfebb5aba8646beae971239e4d3086
Renato Luiz <renatoluiz9834@hotmail.com>
2023-06-26 18:21:58 -0300

    lecture3

---
+++ AulaDjango/lecture3/helo/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, "helo/index.html")
+
+def paulo(request):
+    return HttpResponse("Hello, Paulo!")
+
+def renato(request):
+    return HttpResponse("Hello, Renato!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
